8449df0c2b7efd6ceed0f4d1163b33b816ca48ca
Pulkiiit <96629720+Pulkiiit@users.noreply.github.com>
2024-01-10 14:03:36 +0530

    Update infoHandler.js

--- controllers/infoHandler.js
+++ controllers/infoHandler.js
@@ -1,13 +1,10 @@
 const User = require("../model/user");
 
 const infoHandler = async (req, res, payload) => {
-  const { id } = req.body;
-  if (!id)
-    return res.status(400).json({ message: "Missing required arguments: id" });
-  const info = await User.findOne({ where: { id: id } });
-  if (!info) return res.status(404).json({ message: "no such user" });
-  if (info.username != req.payload)
-    return res.status(400).json({ message: "Unauthorized Access" });
+  const username = req.payload;
+  const info = await User.findOne({ where: { username: username } });
+  if (!info)
+    return res.status(404).json({ message: "No user logged in right now" });
   return res.status(200).json(info);
 };
 
